5859d2880e7083619afc1b796b8b4f73614572e0
1097698475 <1097698475@qq.com>
2021-03-03 22:30:46 +0800

    3月3日

---
+++ 浮点储存（不全/Project25/text25.cpp
@@ -0,0 +1,31 @@
+#define _CRT_SECURE_NO_WARNINGS 
+#include<stdio.h>
+
+
+
+//int main()
+//{
+//	int n = 9;
+//	float *pfloat = (float *)&n;
+//	printf("n的值为：%d\n", n);
+//	printf("pfloat的值为：%f\n", *pfloat);
+//	*pfloat = 9.0;
+//	printf("n的值为：%d\n", n);
+//	printf("pfloat的值为：%f\n", *pfloat);
+//	return 0;
+//}
+//表明整形放，浮点取会有问题
+//浮点放，整形取也有问题
+
+9.0
+1001.0  先转换为二进制
+(-1)^0*1.001*2^3
+(-1)^S*M    *2^E
+S - 0          正数为0，负数为1
+M - 1.001      M为1到2
+E - 3
+储存的时候就是储存S,M,E 见备忘录图片
+
+
+
+数据的存储4 23分钟后没听了
