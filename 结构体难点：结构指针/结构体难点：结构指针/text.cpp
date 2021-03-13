4d6da0d4cdd2f9ed1cf8860bae0ceb9d40143419
1097698475 <1097698475@qq.com>
2021-03-13 11:29:02 +0800

    3月13日

---
+++ 结构体难点：结构指针/结构体难点：结构指针/text.cpp
@@ -0,0 +1,42 @@
+#define _CRT_SECURE_NO_WARNINGS 
+#include<stdio.h>
+#include<math.h>
+#include<stdbool.h>
+
+//下面三种方法是同一个功能
+struct point
+{
+	int x;
+	int y;
+};
+struct point * getstruct(struct point *);
+void output(struct point);
+void print(const struct point*);
+int main()
+{
+	struct point text = { 0,0 };
+	//第一版
+	getstruct(&text);
+	output(text);//思考：一定要分两步吗？
+
+	//第二版优化版
+	output(*getstruct(&text));//解引用getstruct函数返回的p指针，即text结构变量，然后再传到output
+
+	print(getstruct(&text));//给结构修改成员时，传入指针比定义新的结构变量要好：前者可以适用其他函数
+
+}
+struct point * getstruct(struct point *p)
+{
+	scanf("%d", &p->x);
+	scanf("%d", &p->y);
+	printf("您刚才输入了：%d %d\n", p->x, p->y);
+	return p;
+}
+void output(struct point text)
+{
+	printf("打印: %d %d\n", text.x, text.y);
+}
+void print(const struct point *p)
+{
+	printf("print函数打印：%d %d\n", p->x, p->y);
+}
