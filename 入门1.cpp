b616f1a71558d6cb301d47ab7dd0c75c85c4e85c
1097698475 <1097698475@qq.com>
2021-04-16 23:58:39 +0800

    4月16日

---
+++ 入门1.cpp
@@ -0,0 +1,49 @@
+//#include<stdio.h>
+//int main()
+//{
+	//char arr1[]="abc";
+	//char arr2[]={'a','b'};
+	//printf("%s\n",arr1);
+	//printf("%s\n",arr2);
+	
+	
+	
+	//printf("c:\\test\\32\\test");
+	
+	
+//	printf("%c\n",'a');
+//	printf("a\n");
+//	printf("%c\n",'\'');
+//	printf("'\n");
+//	printf("%s\n","abc");
+//	printf("abc\n");
+//	printf("%c\n",'\32');
+//	printf("%c\n",'c');
+//	printf("%c\n",'\c');
+//	printf("%c\n",'\132');
+	//%s,%c  "" ''的用法？
+//	printf("%c\n",'\x32');
+	//字符是用''，字符0对应'\48'即'0' 字符A对应ascii码的65即'\65'='A',字符a对应\97 
+	// 
+//	return 0;
+//}
+
+
+#include<stdio.h>
+int main()
+{
+	int input = 0； 
+	printf("你要好好学习吗？(1/0)  :");
+	scanf("%d\n",&input);
+	if(input==1)
+	printf("走上人生巅峰")；
+	else
+	printf("打工人")； 
+	 
+	 
+	return 0;
+}
+
+
+
+
